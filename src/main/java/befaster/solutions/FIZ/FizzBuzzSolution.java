7d2c6aff63647c2aff812d63ef94e823922a8bf6
sbx_user1051 <sbx_user1051@169.254.75.33>
2019-12-10 17:18:24 +0000

    Tue Dec 10 17:18:24 UTC 2019

--- src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
+++ src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
@@ -6,13 +6,19 @@ public class FizzBuzzSolution {
 
     public String fizzBuzz(Integer number) {
 
+        String aux = String.valueOf(number);
+        char firstChar = aux.charAt(0);
+        String replaced = aux.replaceAll(""+firstChar, "");
+
         if ((number % 3 == 0 || String.valueOf(number).contains("3"))
                 && (number % 5 == 0 || String.valueOf(number).contains("5")
-                && ((number > 10) || String.valueOf(number).matches()))){
-            return "fizz buzz";
+                && ((number > 10) || replaced.length() == 0))){
+            return "fizz buzz deluxe";
         } else if ((number % 3 == 0 || String.valueOf(number).contains("3"))
                 && (number % 5 == 0 || String.valueOf(number).contains("5"))){
             return "fizz buzz";
+        } else if ((number > 10) || replaced.length() == 0) {
+            return "deluxe";
         } else if (number % 3 == 0 || String.valueOf(number).contains("3") ) {
             return "fizz";
         } else if (number % 5 == 0 || String.valueOf(number).contains("5") ) {
@@ -23,5 +29,6 @@ public class FizzBuzzSolution {
     }
 
 }
+
 
 
