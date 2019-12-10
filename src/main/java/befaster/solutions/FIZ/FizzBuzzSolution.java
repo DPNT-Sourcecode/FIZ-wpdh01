0712fa077fe0006d725af5ea05e4bcc87b076335
sbx_user1051 <sbx_user1051@169.254.75.33>
2019-12-10 17:13:11 +0000

    Tue Dec 10 17:13:11 UTC 2019

--- src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
+++ src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
@@ -7,6 +7,10 @@ public class FizzBuzzSolution {
     public String fizzBuzz(Integer number) {
 
         if ((number % 3 == 0 || String.valueOf(number).contains("3"))
+                && (number % 5 == 0 || String.valueOf(number).contains("5")
+                && ((number > 10) || String.valueOf(number).matches()))){
+            return "fizz buzz";
+        } else if ((number % 3 == 0 || String.valueOf(number).contains("3"))
                 && (number % 5 == 0 || String.valueOf(number).contains("5"))){
             return "fizz buzz";
         } else if (number % 3 == 0 || String.valueOf(number).contains("3") ) {
@@ -19,4 +23,5 @@ public class FizzBuzzSolution {
     }
 
 }
+
 
