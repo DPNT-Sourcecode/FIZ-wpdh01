8cdc8605f675d252d9cd927fa48f5acdb5b4d503
sbx_user1051 <sbx_user1051@169.254.75.33>
2019-12-10 18:01:27 +0000

    Tue Dec 10 18:01:27 UTC 2019

--- src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
+++ src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
@@ -6,8 +6,6 @@ public class FizzBuzzSolution {
 
     public String fizzBuzz(Integer number) {
 
-
-
         if (isFizz(number) && isBuzz(number) && isDeluxe(number)){
             if (number%2 != 0)
                 return "fizz buzz fake deluxe";
@@ -35,18 +33,10 @@ public class FizzBuzzSolution {
         }
     }
 
-
-
     public boolean isDeluxe(Integer number){
-//        String aux = String.valueOf(number);
-//        char firstChar = aux.charAt(0);
-//        String replaced = aux.replaceAll(""+firstChar, "");
-//
-//        if (((number > 10) && replaced.length() == 0))
-//            return true;
 
-
-        if ((number % 3 == 0 && containsOneThree(number)) || (number % 5 == 0 && containsOneFive(number)))
+        if ((number % 3 == 0 && containsOneThree(number))
+                || (number % 5 == 0 && containsOneFive(number)))
             return true;
         return false;
 
@@ -65,21 +55,15 @@ public class FizzBuzzSolution {
     }
 
     public boolean containsOneThree(Integer number) {
-        if (String.valueOf(number).contains("3")) {
-            return !String.valueOf(number).replaceFirst("3", "").contains("3");
-        }
+        if (String.valueOf(number).contains("3"))
+            return true;
         return false;
     }
 
     public boolean containsOneFive(Integer number) {
-        if (String.valueOf(number).contains("5")) {
-            return !String.valueOf(number).replaceFirst("5", "").contains("5");
-        }
+        if (String.valueOf(number).contains("5"))
+            return true;
         return false;
 
     }
 }
-
-
-
-
